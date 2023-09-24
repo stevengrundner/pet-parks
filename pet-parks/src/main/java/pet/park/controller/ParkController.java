c5b4c3cf0622eaa5ac46ac42639e09c0c35cedd3
Steven Grundner <133699580+stevengrundner@users.noreply.github.com>
2023-09-24 10:23:03 -0600

    Add files via upload
    14-4  update contributor

--- pet-parks/src/main/java/pet/park/controller/ParkController.java
+++ pet-parks/src/main/java/pet/park/controller/ParkController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -29,6 +30,14 @@ public class ParkController {
 		log.info("Creating contributor {}", contributorData);
 		return parkService.saveContributor(contributorData);
 	}
+
+	@PutMapping("/contributor/{contributorId}")
+	public ContributorData updateContributor(@PathVariable Long contributorId, 
+			@RequestBody ContributorData contributorData) {
+		contributorData.setContributorId(contributorId);
+		log.info("Updating contributor {}", contributorData);
+		return parkService.saveContributor(contributorData);
+	} //(VIDEO 14-4 Update Controller) -> method to update a contributor 
 
 	@GetMapping("/contributor")
 	public List<ContributorData> retriveAllContributors() {
